58a3510f6e6cdae97d8da4b552622aea3a3fc41d
quan200699 <maplestory2012vn@gmail.com>
2022-08-10 18:46:55 +0700

    init project

---
+++ src/main/java/com/codegym/hongheo/HoNgheoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.hongheo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoNgheoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoNgheoProjectApplication.class, args);
+    }
+
+}
