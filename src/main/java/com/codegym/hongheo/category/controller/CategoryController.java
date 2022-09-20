712403b04a32e7224ac1d0515a22736ee573b7da
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-09-20 22:33:58 +0700

    Revert "sửa tính năng delete category"
    This reverts commit 2464079f713246ae5cacdfc3503547d1f5d1eb5a.

--- src/main/java/com/codegym/hongheo/category/controller/CategoryController.java
+++ src/main/java/com/codegym/hongheo/category/controller/CategoryController.java
@@ -54,7 +54,7 @@ public class CategoryController {
     public ResponseEntity<Void> deleteCategory(@PathVariable("id") Long id) {
         Optional<Category> categoryOptional = iCategoryService.findById(id);
         if (categoryOptional.isPresent()) {
-            categoryOptional.get().setStatus(0);
+            iCategoryService.remove(id);
             return new ResponseEntity<>(HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
