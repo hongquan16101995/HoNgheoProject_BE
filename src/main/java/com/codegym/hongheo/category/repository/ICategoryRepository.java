153992e1ef8e7e989336caad93867f6cfef65c76
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-10-11 18:24:02 +0700

    refector controller logic => service logic + call user with Jwt

--- src/main/java/com/codegym/hongheo/category/repository/ICategoryRepository.java
+++ src/main/java/com/codegym/hongheo/category/repository/ICategoryRepository.java
@@ -1,15 +1,18 @@
 package com.codegym.hongheo.category.repository;
 
 import com.codegym.hongheo.category.model.entity.Category;
-import com.codegym.hongheo.core.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
-    List<Category> findAllByUser(User user);
+    @Query(value = "select * from category where user_id = :id",nativeQuery = true)
+    List<Category> findAllByUser(@Param("id") Long id);
 
-    List<Category> findAllByUserAndStatus(User user, int status);
+    @Query(value = "select * from category where user_id = :id and status = 1",nativeQuery = true)
+    List<Category> findAllByUserAndStatus(@Param("id") Long id);
 }
